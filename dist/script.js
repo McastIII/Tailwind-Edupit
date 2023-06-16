4f38fa2aa366b902ef22da489fb4726d1ce53c7a
bethywaps <bethmitra18@gmail.com>
2023-06-16 14:34:51 +0800

    almost finished

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,6 @@
 // toggle menu
 const toggleMenu = document.querySelector(".toggle__menu");
-const mobileNav = document.querySelector(".header__nav");
+const mobileNav = document.querySelector(".header__wrapper ul");
 
 
 toggleMenu.addEventListener("click", () => {
@@ -9,7 +9,7 @@ toggleMenu.addEventListener("click", () => {
 });
 
 //drop down
-const head = document.querySelectorAll(".header__item a");
+const head = document.querySelectorAll(".header__wrapper ul li a");
 head.forEach((drop) => {
   drop.addEventListener("click", () => {
     drop.nextElementSibling.classList.toggle("open");
